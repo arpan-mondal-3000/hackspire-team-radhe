5331e85480baefa04118be369d4027681e448ef7
SandipanPasari <139198423+SandipanPasari@users.noreply.github.com>
2024-07-20 12:36:54 +0530

    home and login created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import React from "react";
 import "./App.css";
-
+import Home from "./Components/Home/Home";
 function App() {
   return (
     <>
+      <Home/>
       <h1>Hello World</h1>
     </>
   );
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return ( 
+        <>
+           <h1>this is home</h1> 
+        </>
+     );
+}
+
+export default Home;
